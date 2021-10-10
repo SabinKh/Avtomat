03493777851859f60b6ee6a4bb343facdadbdaee
SabinKh <91114252+SabinKh@users.noreply.github.com>
2021-10-10 23:50:16 +0400

    Add files via upload

---
+++ ConsoleApp19/Avtomat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp19
+{
+    class Avtomat
+    {
+        public void BirbirAtir(int varolan, int daxilolunan, int atilacaq)
+        {
+            int count = varolan + daxilolunan;
+            if (atilacaq>count)
+            {
+                Console.WriteLine("Atmaq istediyiniz gulle maqazindeki gulleden coxdur");
+
+            }else if (varolan+daxilolunan<30 || varolan+daxilolunan==30)
+            {
+               
+                Console.WriteLine($"Avtomatda var idi:{varolan} gulle. Daxil etdiyimiz gulle:{daxilolunan} Atilan gulle:{atilacaq} Avtomatda qaldi:{count-atilacaq}");
+
+            }
+
+        }
+
+        public void BirdefelikAtir(int varolan,int daxilolunan)
+        {
+            int count = varolan + daxilolunan;
+            if (count>30)
+            {
+                Console.WriteLine("Gulle sayi 30-u kecmemelidir");
+            }
+            if (count<=30)
+            { 
+                Console.WriteLine($"Avtomatda var idi:{varolan} gulle.Daxil etdiniz {daxilolunan} gulle.Atildi {count}");
+            }
+
+        }
+
+
+
+
+
+    }
+
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avtomat silah = new Avtomat();
+            //silah.BirbirAtir(10,12,10);
+            silah.BirdefelikAtir(20,10);
+        }
+    }
+}
